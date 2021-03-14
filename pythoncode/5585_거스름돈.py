e8822824c3a838a8a073707651110fa82980f095
feel0321 <dishate@naver.com>
2021-03-14 22:14:00 +0900

    5585_거스름돈
    5585_거스름돈

---
+++ pythoncode/5585_거스름돈.py
@@ -0,0 +1,22 @@
+change = 1000 - int(input())
+coin = 0
+while change != 0:
+    if change >= 500:
+        coin += change // 500
+        change %= 500
+    elif change >= 100:
+        coin += change // 100
+        change %= 100
+    elif change >= 50:
+        coin += change // 50
+        change %= 50
+    elif change >= 10:
+        coin += change // 10
+        change %= 10
+    elif change >= 5:
+        coin += change // 5
+        change %= 5
+    elif change >= 1:
+        coin += change // 1
+        change %= 1
+print(coin)
