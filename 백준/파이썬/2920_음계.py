815b8dc7e92d90b9d2eec6a69218fa789e8c910b
feel0321 <dishate@naver.com>
2021-04-16 14:07:27 +0900

    백준_2920_음계
    백준_2920_음계

---
+++ 백준/파이썬/2920_음계.py
@@ -0,0 +1,10 @@
+from sys import stdin
+input = stdin.readline
+
+music = list(map(int, input().split()))
+if music == sorted(music):
+    print('ascending')
+elif music == sorted(music, reverse = True):
+    print('descending')
+else:
+    print('mixed')
